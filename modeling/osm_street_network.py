693b23c4b78774bd8d2ce1adcec40a39f1750688
Edi Begovic <edibegovic@gmail.com>
2022-12-06 21:05:16 +0100

    bug-fix in osmnx street network

--- modeling/osm_street_network.py
+++ modeling/osm_street_network.py
@@ -12,54 +12,12 @@ import osmnx as ox
 # Fetch street network from OSM
 # --------------------------------------------------------
 
-# København (Axel Towers)
 axel_towers = Point(12.565886448579562, 55.675641285999056)
-
-tags = {"sidewalk": True} 
-sidewalks = ox.geometries_from_point((axel_towers.y, axel_towers.x), tags=tags, dist=500)
 
 G_walk = ox.graph_from_point((axel_towers.y, axel_towers.x), dist=600, network_type='walk', retain_all=True)
 G_sidewalk = ox.graph_from_point((axel_towers.y, axel_towers.x), dist=600, custom_filter='["sidewalk"]')
 G_full = nx.compose(G_walk, G_sidewalk)
 G = G_full
-
-# --------------------------------------------------------
-# kepler.gl plotting utils
-# --------------------------------------------------------
-
-def extract_line_segments(segments: LineString) -> [Point]:
-    points = [list(p) for p in segments.coords]
-    return [source+target for source, target in zip(points, points[1:])]
-
-def faltten_sidewalk_segments(sidewalks: GeoDataFrame) -> GeoDataFrame:
-    """
-    Flattens a LineString representation of edges, such that there is
-    only one pair of source and target points per row. 
-    We also add seperate columns for each coordinate (needed for kepler.gl plotting)
-    """
-    crs = sidewalks.crs
-    sidewalks['geometry'] = sidewalks['geometry'] \
-                            .apply(extract_line_segments)
-
-    sidewalks = sidewalks.explode('geometry')
-    sidewalks['geometry'] = sidewalks['geometry'] \
-            .apply(lambda x: [(x[0], x[1]), (x[2], x[3])]) \
-            .apply(lambda x: LineString(x))
-
-    sidewalks[['s_lng', 's_lat', 't_lng', 't_lat']] = pd.DataFrame(
-        sidewalks['geometry'].apply(lambda x: (x.coords[0][0], 
-                                               x.coords[0][1], 
-                                               x.coords[1][0], 
-                                               x.coords[1][1])).to_list(), index=sidewalks.index)
-
-    return GeoDataFrame(sidewalks, geometry='geometry', crs=crs)
-
-sidewalks_plot = faltten_sidewalk_segments(sidewalks)
-
-# This is just for keplr.gl
-# (it only shows the first 4 fields in the preview box :)
-sidewalks_plot = sidewalks_plot[['osmid', 'meters_covered', 'percent_covered', 
-    's_lng', 's_lat', 't_lng', 't_lat', 'geometry']]
 
 # --------------------------------------------------------
 # Shade coverage
@@ -98,4 +56,44 @@ sidewalks['meters_covered'] = sidewalks \
 
 sidewalks['percent_covered'] = sidewalks \
         .apply(lambda x: x['meters_covered']/x['length'], 1)
+
+# --------------------------------------------------------
+# kepler.gl plotting utils
+# --------------------------------------------------------
+
+def extract_line_segments(segments: LineString) -> [Point]:
+    points = [list(p) for p in segments.coords]
+    return [source+target for source, target in zip(points, points[1:])]
+
+def faltten_sidewalk_segments(sidewalks: GeoDataFrame) -> GeoDataFrame:
+    """
+    Flattens a LineString representation of edges, such that there is
+    only one pair of source and target points per row. 
+    We also add seperate columns for each coordinate (needed for kepler.gl plotting)
+    """
+    sidewalks = sidewalks.copy()
+    crs = sidewalks.crs
+    sidewalks['geometry'] = sidewalks['geometry'] \
+                            .apply(extract_line_segments)
+
+    sidewalks = sidewalks.explode('geometry')
+    sidewalks['geometry'] = sidewalks['geometry'] \
+            .apply(lambda x: [(x[0], x[1]), (x[2], x[3])]) \
+            .apply(lambda x: LineString(x))
+
+    sidewalks[['s_lng', 's_lat', 't_lng', 't_lat']] = pd.DataFrame(
+        sidewalks['geometry'].apply(lambda x: (x.coords[0][0], 
+                                               x.coords[0][1], 
+                                               x.coords[1][0], 
+                                               x.coords[1][1])).to_list(), index=sidewalks.index)
+
+    return GeoDataFrame(sidewalks, geometry='geometry', crs=crs)
+
+sidewalks_plot = faltten_sidewalk_segments(sidewalks)
+
+# This is just for keplr.gl
+# (it only shows the first 4 fields in the preview box :)
+sidewalks_plot = sidewalks_plot[['osmid', 'meters_covered', 'percent_covered', 
+    's_lng', 's_lat', 't_lng', 't_lat', 'geometry']]
+
 
