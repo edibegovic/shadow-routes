c08fd48255f317c22607264e373f3ede70c083b9
Anita Zielińska <38780197+anitazielinska@users.noreply.github.com>
2022-12-06 12:24:25 +0100

    substract terrain elevation from surface level

--- modeling/preprocess_raster_data.py
+++ modeling/preprocess_raster_data.py
@@ -2,37 +2,46 @@ import os
 import glob
 import rasterio
 from rasterio.merge import merge
+from rasterio.plot import show
+
 
 # --------------------------------------------------------
 # Merge GeoTIFF tiles
 # --------------------------------------------------------
 
 # Path to directory containing GeoTIFF files
-dirpath = r"../data/DHM/DSM_617_72_TIF_UTM32-ETRS89"
-search_criteria = "DSM*.tif"
-q = os.path.join(dirpath, search_criteria)
+def merge_geotiff(dirpath, search_criteria):
+    # dirpath = r"./data/DHM/DSM_617_72_TIF_UTM32-ETRS89"
+    # search_criteria = "DSM*.tif"
+    q = os.path.join(dirpath, search_criteria)
 
-tif_fps = glob.glob(q)
-files_mosaic = [rasterio.open(fp) for fp in tif_fps]
-mosaic, out_trans = merge(files_mosaic, res=25000) # adjust output resolution
+    tif_fps = glob.glob(q)
+    files_mosaic = [rasterio.open(fp) for fp in tif_fps]
+    mosaic, out_trans = merge(files_mosaic) # adjust output resolution
 
-out_meta = src.meta.copy()
-out_meta.update({"driver": "GTiff",
-    "height": mosaic.shape[1],
-    "width": mosaic.shape[2],
-    "transform": out_trans,
-    }
-)
+    src = rasterio.open(tif_fps[-1])
+    out_meta = files_mosaic[0].meta.copy()
+    out_meta.update({"driver": "GTiff",
+        "height": mosaic.shape[1],
+        "width": mosaic.shape[2],
+        "transform": out_trans,
+        }
+    )
 
-out_fp = r"../data/cph_10x10km_mosaic.tif"
-with rasterio.open(out_fp, "w", **out_meta) as dest:
-    dest.write(mosaic)
+    return mosaic, out_meta
 
-# --------------------------------------------------------
-# Subtract terrain elevation from surface level elevation
-# --------------------------------------------------------
+dsm_mosaic, dsm_meta = merge_geotiff(r"./data/DHM/DSM_617_72_TIF_UTM32-ETRS89",  "DSM*.tif")
+dtm_mosaic, dtm_meta = merge_geotiff(r"./data/DHM/DTM_617_72_TIF_UTM32-ETRS89",  "DTM*.tif")
 
-## TODO
-# DANMARKS HØJDEMODEL TERRÆN 
-# DANMARKS HØJDEMODEL OVERFLADE 
+if dsm_meta == dtm_meta:
+    mosaic = dsm_mosaic - dtm_mosaic
+    out_meta = dsm_meta
+    out_fp = r"./data/cph_10x10km_mosaic.tif"
+    with rasterio.open(out_fp, "w", **out_meta) as dest:
+        dest.write(mosaic)
+    show(mosaic)
+else:
+    raise Exception("Unmatching metadata of subtracted files")
+
+
 
